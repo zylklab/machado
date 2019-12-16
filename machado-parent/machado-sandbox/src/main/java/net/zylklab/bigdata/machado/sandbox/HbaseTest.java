38475aea07e0392288096ddff1fbe909f0dd2470
gfg001 <gus@zylk.net>
2019-12-16 18:31:55 +0100

    get & put for the test

--- machado-parent/machado-sandbox/src/main/java/net/zylklab/bigdata/machado/sandbox/HbaseTest.java
+++ machado-parent/machado-sandbox/src/main/java/net/zylklab/bigdata/machado/sandbox/HbaseTest.java
@@ -1,21 +1,19 @@
 package net.zylklab.bigdata.machado.sandbox;
 
-import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 import java.util.UUID;
 
+import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.beryx.textio.TextIO;
 import org.beryx.textio.TextIoFactory;
 import org.beryx.textio.TextTerminal;
 
 import net.zylklab.bigdata.machado.hbase.HBaseManager;
-import net.zylklab.bigdata.machado.hbase.pool.HBasePoolException;
 
 public class HbaseTest {
 
@@ -37,17 +35,32 @@ public class HbaseTest {
 		String key = partition + UUID.randomUUID();
 		return key.getBytes();
 	}
-	private Put buildPut(String partition) {
-		Put put =  new Put(buildKey(partition));
+	private Put buildPut(byte[] key) {
+		Put put =  new Put(key);
 		put.addColumn(FAMILY_BASE, QUALIFIER_FAMILY_BASE_DATE, Bytes.toBytes(this.df.format(new Date(System.currentTimeMillis()))));
 		put.addColumn(FAMILY_EXTRA, QUALIFIER_FAMILY_EXTRA_TOTAL, Bytes.toBytes(System.currentTimeMillis()));
 		return put;
 	}
 	
-	private void puts(String partition, int numberOfPuts, int msbetweenPuts) throws IOException, HBasePoolException, InterruptedException {
+	private Get buildGet(byte[] key) {
+		Get get =  new Get(key);
+		get.addColumn(FAMILY_BASE, QUALIFIER_FAMILY_BASE_DATE);
+		get.addColumn(FAMILY_EXTRA, QUALIFIER_FAMILY_EXTRA_TOTAL);
+		return get;
+	}
+	
+	private void puts(String partition, int numberOfPuts, int msbetweenPuts) {
 		for (int i = 0; i < numberOfPuts; i++) {
-			Thread.sleep(msbetweenPuts);
-			HBaseManager.put(buildPut(partition), TABLE);
+			try {
+				Thread.sleep(msbetweenPuts);
+				byte[] key = buildKey(partition);
+				HBaseManager.put(buildPut(key), TABLE);
+				Result result = HBaseManager.get(buildGet(key), TABLE);
+				String date = Bytes.toString(result.getValue(FAMILY_BASE, QUALIFIER_FAMILY_BASE_DATE));
+				System.out.println(date);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
@@ -58,16 +71,11 @@ public class HbaseTest {
 		while(run == SINO.Y) {
 			TextIO textIO = TextIoFactory.getTextIO();
 			String partition = textIO.newStringInputReader().withDefaultValue("0").read("In what partition (from 0-9) want to write data?");
-			Integer numberOfPuts = textIO.newIntInputReader().withDefaultValue(1).read("Number of puts?");
-			Integer msbetweenPuts = textIO.newIntInputReader().withDefaultValue(0).read("ms between puts?");
+			Integer numberOfPuts = textIO.newIntInputReader().withDefaultValue(1).read("Number of puts & gets?");
+			Integer msbetweenPuts = textIO.newIntInputReader().withDefaultValue(0).read("ms between puts & gets?");
 			TextTerminal<?> terminal = textIO.getTextTerminal();
 			terminal.printf("\nWriting %d puts at Partition %s \n", numberOfPuts, partition);
-			try {
-				test.puts(partition,numberOfPuts, msbetweenPuts);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			
+			test.puts(partition,numberOfPuts, msbetweenPuts);
 			run = textIO.newEnumInputReader(SINO.class).withDefaultValue(SINO.Y).read("Do you want to do another put group? (Y/N)");
 		}
 		System.exit(0); 
